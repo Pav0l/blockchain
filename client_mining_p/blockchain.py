7054f5fec28b13a2959dd688fafbbc0c9f01dbee
Pav0l <pavol.karas@gmail.com>
2019-07-10 15:46:06 +0200

    Modify /mine endpoint to check for valid proof

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -141,28 +141,41 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['POST'])
 def mine():
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mine a new coin
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+    body = request.get_json()
 
-    # Forge the new BLock by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+    # Check that the required fields are in the POST'ed data
+    required = ['last_proof', 'proof']
+    if not all(k in body for k in required):
+        return 'Missing proof', 400
 
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
+    if blockchain.valid_proof(body['last_proof'], body['proof']):
+        # We must receive a reward for finding the proof.
+        # The sender is "0" to signify that this node has mine a new coin
+        blockchain.new_transaction(
+            sender="0",
+            recipient=node_identifier,
+            amount=1,
+        )
+
+        # Forge the new BLock by adding it to the chain
+        previous_hash = blockchain.hash(blockchain.last_block)
+        block = blockchain.new_block(body['proof'], previous_hash)
+
+        response = {
+            'message': "New Block Forged",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash'],
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': "Invalid proof"
+        }
+        return jsonify(response), 401
 
 
 @app.route('/transactions/new', methods=['POST'])
